9aa72a37f53dc9a51d7596da22e40c18dc7101df
Bill Kuehler <wkuehler@gmail.com>
2021-08-10 08:34:33 -0400

    Added LWC

--- force-app/main/default/lwc/gridTestLWC/gridTestLWC.js
+++ force-app/main/default/lwc/gridTestLWC/gridTestLWC.js
@@ -2,5 +2,8 @@ import { LightningElement } from "lwc";
 
 export default class GridTestLWC extends LightningElement {
    rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-   columns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   athroughz = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   dthroughz = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+   dthroughm = ["d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+   nthroughz = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 }

f37782fc7eb50c466b57ea57967f4b842f31464e
Bill Kuehler <wkuehler@gmail.com>
2021-08-10 08:09:07 -0400

    LWC base

---
+++ force-app/main/default/lwc/gridTestLWC/gridTestLWC.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+
+export default class GridTestLWC extends LightningElement {
+   rows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+   columns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+}
